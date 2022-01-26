9ae4aba2eea0d05c35ef3dbcdf03f621434b909a
Boilingbrains <68190528+boilingbrains@users.noreply.github.com>
2022-01-27 00:17:03 +0100

    Add files via upload

---
+++ MPU6050_GTA.ino
@@ -0,0 +1,127 @@
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "Wire.h"
+#include "Keyboard.h"
+
+MPU6050 mpu;
+
+#define INTERRUPT_PIN 7  // use pin 2 on Arduino Uno & most boards
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+void setup() {
+  Wire.begin();
+
+  Keyboard.begin();
+  //Serial.begin(115200);
+  //while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+  // initialize device
+  //Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  // verify connection
+  //Serial.println(F("Testing device connections..."));
+  //Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // load and configure the DMP
+  //Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    //Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    //Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    //Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    //Serial.println(F(")..."));
+    //attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    //Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    //Serial.print(F("DMP Initialization failed (code "));
+    //Serial.print(devStatus);
+    //Serial.println(F(")"));
+  }
+}
+
+void loop() {
+  if (!dmpReady) return;
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    //Serial.print("ypr\t");
+    //Serial.print(ypr[0] * 180 / M_PI);
+    //Serial.print("\t");
+    //Serial.print(ypr[1] * 180 / M_PI);
+    //Serial.print("\t");
+    //Serial.println(ypr[2] * 180 / M_PI);
+
+    if ((ypr[1] * 180 / M_PI) >= 8)
+    {
+      Keyboard.press('w'); //w en QWERTY donne z en AZERTY
+      //Serial.println("z");
+    }
+    if ((ypr[1] * 180 / M_PI) <= -8)
+    {
+      Keyboard.press('s');
+      //Serial.println("s");
+    }
+    if ((ypr[2] * 180 / M_PI) >= 10)
+    {
+      Keyboard.press('a'); //a en QWERTY donne q en AZERTY
+      //Serial.println("q");
+    }
+    if ((ypr[2] * 180 / M_PI) <= -10)
+    {
+      Keyboard.press('d');
+      //Serial.println("d");
+    }
+    delay(50);
+    Keyboard.releaseAll();
+    delay(1);
+  }
+}
